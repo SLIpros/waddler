ed784724173a522da0c8853a6209efb4a569b0bd
rtbe <kondr.egserg@gmail.com>
2023-07-03 21:17:08 +0500

    tests: refactor slice_test.go

--- value/slice_test.go
+++ value/slice_test.go
@@ -9,124 +9,197 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSetSliceString(t *testing.T) {
-	t.Run("String", func(t *testing.T) {
-		sl := []string{str, str}
+func TestSetSliceString1(t *testing.T) {
+	type args struct {
+		field     *reflect.Value
+		arr       []string
+		equalFunc func()
+	}
 
-		var testStruct struct {
-			S string
-		}
+	tests := []struct {
+		name    string
+		args    func() args
+		wantErr bool
+	}{
+		{
+			name: "String",
+			args: func() args {
+				var testStruct struct {
+					S string
+				}
 
-		v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				var field reflect.Value
+				v := reflect.Indirect(reflect.ValueOf(&testStruct))
 
-		for i := 0; i < v.NumField(); i++ {
-			fieldValue := v.Field(i)
-			err := SetSliceString(&fieldValue, sl)
-			require.NoError(t, err)
-			require.Equal(t, strings.Join(sl, ","), testStruct.S)
-		}
-	})
+				for i := 0; i < v.NumField(); i++ {
+					field = v.Field(i)
+				}
 
-	t.Run("[]string", func(t *testing.T) {
-		sl := []string{str, str}
+				sl := []string{str, str}
 
-		var testStruct struct {
-			SL []string
-		}
+				return args{
+					field: &field,
+					arr:   sl,
+					equalFunc: func() {
+						require.Equal(t, strings.Join(sl, ","), field.String())
+					},
+				}
+			},
+		},
+		{
+			name: "[]string",
+			args: func() args {
+				sl := []string{str, str}
 
-		v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				var testStruct struct {
+					SL []string
+				}
 
-		for i := 0; i < v.NumField(); i++ {
-			fieldValue := v.Field(i)
-			err := SetSliceString(&fieldValue, sl)
-			require.NoError(t, err)
-			require.Equal(t, sl, testStruct.SL)
-		}
-	})
+				var field reflect.Value
+				v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				for i := 0; i < v.NumField(); i++ {
+					field = v.Field(i)
+				}
 
-	t.Run("[]any", func(t *testing.T) {
-		sl := []string{str, str}
+				return args{
+					field: &field,
+					arr:   sl,
+					equalFunc: func() {
+						require.Equal(t, sl, testStruct.SL)
+					},
+				}
+			},
+		},
+		{
+			name: "[]any",
+			args: func() args {
+				var testStruct struct {
+					SL []any
+				}
 
-		var testStruct struct {
-			SL []any
-		}
+				var field reflect.Value
+				v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				for i := 0; i < v.NumField(); i++ {
+					field = v.Field(i)
+				}
 
-		v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				sl := []string{str, str}
+				return args{
+					field: &field,
+					arr:   sl,
+					equalFunc: func() {
+						require.Equal(t, []any{str, str}, testStruct.SL)
+					},
+				}
+			},
+		},
+		{
+			name: "[]string in any",
+			args: func() args {
+				var testStruct struct {
+					SL any
+				}
 
-		for i := 0; i < v.NumField(); i++ {
-			fieldValue := v.Field(i)
-			err := SetSliceString(&fieldValue, sl)
-			require.NoError(t, err)
-			require.Equal(t, []any{str, str}, testStruct.SL)
-		}
-	})
+				testStruct.SL = make([]string, 0)
 
-	/*
-		t.Run("[]string in any", func(t *testing.T) {
-			sl := []string{str, str}
+				var field reflect.Value
+				v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				for i := 0; i < v.NumField(); i++ {
+					field = v.Field(i)
+				}
 
-			var testStruct struct {
-				Str any
+				sl := []string{str, str}
+				return args{
+					field: &field,
+					arr:   sl,
+					equalFunc: func() {
+						require.Equal(t, []string{str, str}, testStruct.SL)
+					},
+				}
+			},
+		},
+		{
+			name:    "[]error",
+			wantErr: true,
+			args: func() args {
+				var testStruct struct {
+					SL []error
+				}
+
+				var field reflect.Value
+				v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				for i := 0; i < v.NumField(); i++ {
+					field = v.Field(i)
+				}
+
+				sl := []string{str, str}
+				return args{
+					field: &field,
+					arr:   sl,
+				}
+			},
+		},
+		{
+			name:    "error",
+			wantErr: true,
+			args: func() args {
+				var testStruct struct {
+					Err error
+				}
+
+				testStruct.Err = errors.New("")
+
+				var field reflect.Value
+				v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				for i := 0; i < v.NumField(); i++ {
+					field = v.Field(i)
+				}
+				sl := []string{str, str}
+
+				return args{
+					field: &field,
+					arr:   sl,
+				}
+			},
+		},
+		{
+			name:    "map[string]string",
+			wantErr: true,
+			args: func() args {
+				sl := []string{str, str}
+
+				var testStruct struct {
+					M map[string]string
+				}
+
+				testStruct.M = make(map[string]string)
+
+				var field reflect.Value
+				v := reflect.Indirect(reflect.ValueOf(&testStruct))
+				for i := 0; i < v.NumField(); i++ {
+					field = v.Field(i)
+				}
+
+				return args{
+					field: &field,
+					arr:   sl,
+				}
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			args := tt.args()
+			err := SetSliceString(args.field, args.arr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SetSliceString() error = %v, wantErr %v", err, tt.wantErr)
 			}
-
-			v := reflect.Indirect(reflect.ValueOf(&testStruct))
-
-			for i := 0; i < v.NumField(); i++ {
-				fieldValue := v.Field(i)
-				err := SetSliceString(&fieldValue, sl)
-				require.NoError(t, err)
-				require.Equal(t, sl, testStruct.Str)
+			if args.equalFunc != nil {
+				args.equalFunc()
+			}
+			if tt.wantErr {
+				require.Error(t, err)
 			}
 		})
-	*/
-
-	t.Run("[]error", func(t *testing.T) {
-		sl := []string{str, str}
-
-		var testStruct struct {
-			SL []error
-		}
-
-		v := reflect.Indirect(reflect.ValueOf(&testStruct))
-
-		for i := 0; i < v.NumField(); i++ {
-			fieldValue := v.Field(i)
-			err := SetSliceString(&fieldValue, sl)
-			require.Error(t, err)
-		}
-	})
-
-	t.Run("not assignable interface", func(t *testing.T) {
-		sl := []string{str, str}
-
-		var testStruct struct {
-			Err error
-		}
-
-		testStruct.Err = errors.New("")
-
-		v := reflect.Indirect(reflect.ValueOf(&testStruct))
-
-		for i := 0; i < v.NumField(); i++ {
-			fieldValue := v.Field(i)
-			err := SetSliceString(&fieldValue, sl)
-			require.Error(t, err)
-		}
-	})
-
-	t.Run("Unsupported", func(t *testing.T) {
-		sl := []string{str, str}
-
-		var testStruct struct {
-			M map[string]string
-		}
-
-		v := reflect.Indirect(reflect.ValueOf(&testStruct))
-
-		for i := 0; i < v.NumField(); i++ {
-			fieldValue := v.Field(i)
-			err := SetSliceString(&fieldValue, sl)
-			require.Error(t, err)
-		}
-	})
+	}
 }
